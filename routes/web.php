1e8c1df3f6e613dc25c8bc8a531b88fa6e10db89
EmreYamanYazilim <111569041+EmreYamanYazilim@users.noreply.github.com>
2024-09-03 14:33:57 +0300

    Site parçalama işlemi yapıldı

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\CardController;
+use App\Http\Controllers\CheckoutController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\MyOrdersController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/',[FrontController::class,'index']);
+Route::get('/urun-listesi', [ProductController::class, 'list']);
+Route::get('/urun-detay',[ProductController::class, 'detail']);
+Route::get('/sepet', [CardController::class,'card']);
+Route::get('/odeme',[CheckoutController::class,'index']);
+Route::get('/siparislerim',[MyOrdersController::class,'index']);
+Route::get('/siperislerim-detay',[MyOrdersController::class,'detail']);
